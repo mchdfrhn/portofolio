d58794a49c9e86c9b26337b4cbcf74e24ba64383
Mochammad Farhan Ali <mochamadfarhanali@gmail.com>
2026-04-27 17:13:27 +0700

    feat: implement Keystatic CMS, add content collections, and configure project deployment dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
+import keystatic from "@keystatic/astro";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
+    keystatic(),
   ],
 });
